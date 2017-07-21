9474097dbb9b794fafb396dad88ed511bfdbc4c3
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-07-21 20:22:17 +0100

    seems to work, will try with another lookup before moving it into the player's thing

--- client/src/models/lookup.js
+++ client/src/models/lookup.js
@@ -1,11 +1,21 @@
 var Lookup = function( betToCheck ) {
   this.betToCheck = betToCheck;
+  this.multiplication = 0;
 }
 
 Lookup.prototype = {
 
   seeBetToCheck: function() {
     return this.betToCheck;
+  },
+
+  getMultiplication: function() {
+    switch( this.betToCheck ) {
+      case 2:
+        this.multiplication = 17;
+        break;
+    }
+    return this.multiplication;
   }
 }
 
