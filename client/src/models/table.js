332e1f9a9d4c7e8cd4f0d9aa594a956ef1ba2e4d
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-07-21 22:16:52 +0100

    changing the table definition

--- client/src/models/table.js
+++ client/src/models/table.js
@@ -4,6 +4,20 @@ var Table = function() {
                     15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
                     30, 31, 32, 33, 34, 35, 36 ];
 
+  this.row0 =   [  0 ];
+  this.row1 =   [  1,  2,  3 ];
+  this.row2 =   [  4,  5,  6 ];
+  this.row3 =   [  7,  8,  9 ];
+  this.row4 =   [ 10, 11, 12 ];
+  this.row5 =   [ 13, 14, 15 ];
+  this.row6 =   [ 16, 17, 18 ];
+  this.row7 =   [ 19, 20, 21 ];
+  this.row8 =   [ 22, 23, 24 ];
+  this.row9 =   [ 25, 26, 27 ];
+  this.row10 =  [ 28, 29, 30 ];
+  this.row11 =  [ 31, 32, 33 ];
+  this.row12 =  [ 34, 35, 36 ];
+
 }
 
 Table.prototype = {
