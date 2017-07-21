42784722482ed99c3037139277e7ec96448074a7
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-07-21 23:03:37 +0100

    a bit closer, still not perfect, might need to store the numbers as text and then convert them

--- client/src/views/MainView.js
+++ client/src/views/MainView.js
@@ -12,16 +12,21 @@ MainView.prototype = {
     var tableSpace = document.getElementById( 'table-space' );
 
     var tableList = document.createElement( 'ul' );
+    tableList.style.display = 'table';
+    tableList.style.maxWidth = '100%';
+    tableList.style.textAlign = 'center';
 
     for( var i = 0; i < table.allRows.length; i++ ) {
 
       var row = document.createElement( 'ul' );
+      row.style.width = '100%';
       row.style.display = 'table';
+      row.style.textAlign = 'center';
 
       for( var j = 0; j < table.seeRow( i ).length; j++ ) {
         var number = document.createElement( 'li' );
 
-        number.style.float = 'left';
+        number.style.display = 'inline';
         number.innerText = table.seeRow( i )[ j ];
         number.id = table.seeRow( i )[ j ];
         number.onclick = function( e ) {
