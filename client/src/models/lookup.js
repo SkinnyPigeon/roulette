f211950bcf03c905671bb627bea6fedaacc2a1a9
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-07-21 20:48:11 +0100

    lookup table looks complete

--- client/src/models/lookup.js
+++ client/src/models/lookup.js
@@ -11,11 +11,26 @@ Lookup.prototype = {
 
   getMultiplication: function() {
     switch( this.betToCheck ) {
+      case 1:
+        this.multiplication = 35;
+        break;
       case 2:
         this.multiplication = 17;
         break;
       case 3:
         this.multiplication = 11;
+        break;
+      case 4:
+        this.multiplication = 8;
+        break;
+      case 6:
+        this.multiplication = 5;
+        break;
+      case 12:
+        this.multiplication = 2;
+        break;
+      case 18:
+        this.multiplication = 1;
         break;
     }
     return this.multiplication;
